45b82ea530745566c899970b53abf3aecdf2621f
MattCaleb165 <chm165@live.com>
2023-05-16 21:39:38 -0400

    updated the code to work better/more functional

--- Codecademy_Projects/banking_example.rb
+++ Codecademy_Projects/banking_example.rb
@@ -1,44 +1,86 @@
 class Account
-  attr_reader :name, :balance
-  
-  def initialize(name, balance=100)
+  attr_reader :name, :balance, :pin_number
+  attr_writer :choice
+
+  def initialize(name, balance, pin_number, choice)
     @name = name
     @balance = balance
+    @pin_number = pin_number
+    @choice = choice
   end
 
-  private def pin
+  private
+
+  def pin
     @pin = 1234
   end
 
-  private def pin_error
-    return "Access denied: incorrect PIN."
+  def pin_error
+    "Access denied: incorrect PIN."
   end
 
-  public def display_balance(pin_number)
-    if pin_number == pin
+  public
+
+  def display_balance
+    puts "Enter your PIN: "
+    entered_pin = gets.chomp.to_i
+
+    if entered_pin == pin && @choice == "Display"
       puts "Balance: $#{@balance}."
     else
       puts pin_error
     end
   end
 
-  public def withdraw(pin_number, amount)
-    if pin_number == pin && amount <= @balance
-      @balance - amount
-      puts "Withdrew #{amount}. New balance: $#{@balance}."
+  def withdraw
+    puts "Enter your PIN: "
+    entered_pin = gets.chomp.to_i
+
+    if entered_pin == pin
+      puts "Enter the amount to withdraw: "
+      amount = gets.chomp.to_i
+
+      if amount <= @balance
+        @balance -= amount
+        puts "Withdrew #{amount}. New balance: $#{@balance}."
+      else
+        puts "Insufficient funds."
+      end
     else
       puts pin_error
     end
   end
 
-  public def deposit(pin_number, amount)
-    if pin_number == pin
-      @balance + amount
-      puts "Deposited #{amount}. New Balance: $#{@balance}."
+  def deposit
+    puts "Enter your PIN: "
+    entered_pin = gets.chomp.to_i
+
+    if entered_pin == pin
+      puts "Enter the amount to deposit: "
+      amount = gets.chomp.to_i
+
+      @balance += amount
+      puts "Deposited #{amount}. New balance: $#{@balance}."
     else
       puts pin_error
     end
   end
 end
 
-checking_account = Account.new("deposit", 1000)
+puts "Hello and welcome to the Ruby Gem Bank."
+puts "What would you like to do today?\nDisplay?\nWithdraw?\nDeposit? "
+choice = gets.chomp
+
+checking_account = Account.new("Test", 1000, 1234, choice)
+
+if choice == "Display"
+  checking_account.display_balance
+elsif choice == "Withdraw"
+  checking_account.withdraw
+elsif choice == "Deposit"
+  checking_account.deposit
+else
+  puts "Invalid choice."
+end
+
+
